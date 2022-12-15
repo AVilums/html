5ffdde2bef6c32257b58863802e61edb24196405
Artūrs Viļums <arturvilum@gmail.com>
2022-12-15 20:37:11 +0200

    extra refactoring

--- studiju_projekts/csv.js
+++ studiju_projekts/csv.js
@@ -21,22 +21,6 @@ function stringTo2dArray(string, d1, d2) {
     return string.split(d1).map(function (x) { return x.split(d2) });
 }
 
-function readFileAsText(file) {
-    return new Promise(function (resolve, reject) {
-        let fr = new FileReader();
-
-        fr.onload = function () {
-            resolve(fr.result);
-        };
-
-        fr.onerror = function () {
-            reject(fr);
-        };
-
-        fr.readAsText(file);
-    });
-}
-
 function forpush(b) {
     for (let i = 1; i < b.length; i++) {
         struktura.push(b[i][1]); //strukturu nosaukumi
---
+++ studiju_projekts/file.js
@@ -0,0 +1,15 @@
+function readFileAsText(file) {
+    return new Promise(function (resolve, reject) {
+        let fr = new FileReader();
+
+        fr.onload = function () {
+            resolve(fr.result);
+        };
+
+        fr.onerror = function () {
+            reject(fr);
+        };
+
+        fr.readAsText(file);
+    });
+}
