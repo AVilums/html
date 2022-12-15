213afe53cefdc6afffbeb7d5c00d8c464c5db613
Artūrs Viļums <arturvilum@gmail.com>
2022-12-15 20:31:21 +0200

    izvadits.html - cleaned up

--- studiju_projekts/csv.js
+++ studiju_projekts/csv.js
@@ -13,10 +13,34 @@ function makeTableHTML(myArray) {
 }
 
 function arrayToHTML() {
-    var result = makeTableHTML(strukturasViss);
+    var result = makeTableHTML(myArray);
     document.write(result);
 }
 
 function stringTo2dArray(string, d1, d2) {
     return string.split(d1).map(function (x) { return x.split(d2) });
+}
+
+function readFileAsText(file) {
+    return new Promise(function (resolve, reject) {
+        let fr = new FileReader();
+
+        fr.onload = function () {
+            resolve(fr.result);
+        };
+
+        fr.onerror = function () {
+            reject(fr);
+        };
+
+        fr.readAsText(file);
+    });
+}
+
+function forpush(b) {
+    for (let i = 1; i < b.length; i++) {
+        struktura.push(b[i][1]); //strukturu nosaukumi
+        strukturaId.push(b[i][0]); //strukturu ID nosaukumi
+    }
+    myArray = b;
 }
